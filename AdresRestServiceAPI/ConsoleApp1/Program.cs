15eac46df6ba8c20a7ff89a3cbbacbffc01286b8
Ali-Maes <79624543+AliMa3s@users.noreply.github.com>
2021-11-09 03:25:34 +0100

    Add files via upload

---
+++ AdresRestServiceAPI/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using DataLayer.Repositories;
+using DataLayer.REpositories;
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string cs = @"Data Source=NB21-6CDPYD3\SQLEXPRESS;Initial Catalog=Adresbeheer2B;Integrated Security=True";
+            GemeenteRepositoryADO repo = new GemeenteRepositoryADO(cs);
+            var x=repo.GeefGemeente(10000);
+            Console.WriteLine(x);
+            StraatRepositoryADO rs = new StraatRepositoryADO(cs);
+
+            foreach (var s in rs.GeefStratenGemeente(10000))
+                Console.WriteLine(s);
+        }
+    }
+}
